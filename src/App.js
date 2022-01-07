b8ba872455650a4e1e7aeb2fbe7c4cebabbd1390
hdh <hdh@onlinepowers.com>
2022-01-07 11:00:19 +0900

    [git] 생명주기 함수

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
 import React from 'react';
-import propTypes from 'prop-types';
 
-function Food({name, picture,rate}) {
-  
-  return (
-    <div>
-      <h2>i like {name}</h2>
-      <span>평점 : {rate} / 5.0</span>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-const foodLike = [
-  {
-    id:1,
-    name: 'soju1',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.mCwexplUrwjectbne65Y0QHaHa%26pid%3DApi%26h%3D160&f=1',
-    rating: 4.3,
-  },
-  {
-    id:2,
-    name: 'soju2',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.XYZFxmRo6It6KgdAu2w91wHaHa%26pid%3DApi%26h%3D160&f=1',
-    rating: 4.8,
-  },
-  {
-    id:3,
-    name: 'soju3',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Zj8GCJg2E9I-Horuc6EQmQHaFj%26pid%3DApi%26h%3D160&f=1',
-    rating: 4.9,
-  },
-]
- 
-// function rederFood (dish) {
-//   return <Food name={dish.name} picture={dish.image}/>
-// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor")
+  };
 
-// 화살표 함수 방식
-const renderFood = dish => <Food key={dish.id} name={dish.name} picture={dish.image} rate={dish.rating}/>
+  componentDidMount() {
+    console.log('DidMount')
+  }
+  componentDidUpdate() {
+    console.log('update')
+  }
+  componentWillUnmount() {
+    console.log('cruel world')
+  }
+  state = {
+    count: 0,
+  };
 
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }))
+  };
 
-function App() {
-  
-  return (
-    <div>
-      {foodLike.map(renderFood)}
-    </div>
-  );
-}
+  minus = () => {
+    this.setState(current => ({
+      count: current.count -1,
+    }))
+  };
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rate: propTypes.number.isRequired
+  render() {
+    console.log("I'm rendering")
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
